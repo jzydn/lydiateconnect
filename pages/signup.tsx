7ad5212906d97e5d283e2146af24f9ca65ed5f2d
jay <161174861+jzydn@users.noreply.github.com>
2024-06-21 16:37:57 +0100

    Update signup.tsx

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -80,7 +80,7 @@ const SignUp: NextPage = ({ }) => {
 				<div>
 					<p className="font-bold text-2xl ">Create an account</p>
 					<p className="text-md text-gray-500 dark:text-gray-200">
-						Create a new account for this group's Tovy
+						Create a new account for this group's LYDIATE Connect
 					</p>
 					<FormProvider {...methods}>
 						<form className="mb-8 mt-2" onSubmit={handleSubmit(nextSlide)}>
@@ -100,10 +100,10 @@ const SignUp: NextPage = ({ }) => {
 				</div>
 				<div>
 					<p className="font-bold text-2xl" id="2">
-						Make your Tovy account
+						Make your LYDIATE Connect account
 					</p>
 					<p className="text-md text-gray-500 dark:text-gray-200">
-						You need to create a Tovy account to continue
+						You need to create a LYDIATE Connect account to continue
 					</p>
 					<FormProvider {...signupform}>
 						<form className="mb-8 mt-2" onSubmit={handleSubmit(createAccount)}>
@@ -126,7 +126,7 @@ const SignUp: NextPage = ({ }) => {
 						</Button>
 					</div>
 					<div className="w-full flex">
-						<a className="pt-4 mx-auto  text-gray-400 hover:underline cursor-pointer hover:text-blue-600 transition " href="https://tovyblox.xyz"> © Tovy 2022 </a>
+						<a className="pt-4 mx-auto  text-gray-400 hover:underline cursor-pointer hover:text-blue-600 transition " href="https://LYDIATE Connectblox.xyz"> © LYDIATE Connect 2022 </a>
 					</div>
 				</div>
 				<div>
@@ -162,7 +162,7 @@ const SignUp: NextPage = ({ }) => {
 						</button>
 					</div>
 					<div className="w-full flex">
-						<a className="pt-4 mx-auto  text-gray-400 hover:underline cursor-pointer hover:text-blue-600 transition " href="https://tovyblox.xyz"> © Tovy 2022 </a>
+						<a className="pt-4 mx-auto  text-gray-400 hover:underline cursor-pointer hover:text-blue-600 transition " href="https://LYDIATE Connectblox.xyz"> © LYDIATE Connect 2022 </a>
 					</div>
 				</div>
 			</Slider>
